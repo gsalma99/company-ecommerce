76b6ed2c5f3a6c85323f469397c4ebfa6b4233e8
gsalma99 <salmagonzalezzz10011@gmail.com>
2023-01-01 21:58:04 -0600

    puhsing the api

---
+++ Develop/routes/api/tag-routes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+      }
+    ]
+  })
+  .then(data => res.status(200).json(data))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+  // find all tags
+  // be sure to include its associated Product data
+});
+
+router.get('/:id', (req, res) => {
+  Tag.findByPk(req.params.id, {
+    include: [
+      {
+        model: Product,
+        through: ProductTag
+      }
+    ]
+  })
+  .then(data => res.status(200).json(data))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+});
+
+module.exports = router;
